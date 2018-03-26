ca892b32f34e3dff363a85effe44249c8a735f08
Klaus Meyer <spam@klaus-meyer.net>
2018-03-26 19:05:32 +0200

    remove mysql example data and don't attempt do define root pw

--- provisioning/roles/mysql/files/mailserver.sql
+++ provisioning/roles/mysql/files/mailserver.sql
@@ -24,20 +24,3 @@ CREATE TABLE IF NOT EXISTS `virtual_aliases` (
   PRIMARY KEY (`id`),
   FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT IGNORE INTO `virtual_domains`
-  (`id` ,`name`)
-VALUES
-  ('1', 'example.com'),
-  ('2', 'hostname.example.com');
-
-INSERT IGNORE INTO `virtual_users`
-  (`id`, `domain_id`, `password` , `email`)
-VALUES
-  ('1', '1', ENCRYPT('firstpassword', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'email1@example.com'),
-  ('2', '1', ENCRYPT('secondpassword', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'email2@example.com');
-
-INSERT IGNORE INTO `virtual_aliases`
-  (`id`, `domain_id`, `source`, `destination`)
-VALUES
-  ('1', '1', 'alias@example.com', 'email1@example.com');
